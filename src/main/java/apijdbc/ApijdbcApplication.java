840658cf3d0938e6eba0bba887c2678bafa117b9
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-24 12:08:34 -0400

    first commit

---
+++ src/main/java/apijdbc/ApijdbcApplication.java
@@ -0,0 +1,13 @@
+package apijdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApijdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApijdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apijdbc/ApijdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package apijdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApijdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
